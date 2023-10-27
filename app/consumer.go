d357507f2b1357b5c73ca290f86618a5eeffb059
emmanuelviniciusdev <emmanuelviniciusdev@gmail.com>
2023-10-27 11:08:03 -0300

    imersao-fullcycle-apache-kafka

--- app/consumer.go
+++ app/consumer.go
@@ -1,1 +1,35 @@
 package app
+
+import (
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func NewKafkaConsumer(topics []string, clientID string, groupID string) {
+	configMap := &kafka.ConfigMap{
+		"bootstrap.servers": "localhost:9092",
+		"client.id":         clientID,
+		"group.id":          groupID,
+		//"auto.offset.reset": "earliest",
+	}
+
+	consumer, err := kafka.NewConsumer(configMap)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = consumer.SubscribeTopics(topics, nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		message, err := consumer.ReadMessage(-1)
+
+		if err == nil {
+			fmt.Println(string(message.Value), message.TopicPartition)
+		}
+	}
+}
