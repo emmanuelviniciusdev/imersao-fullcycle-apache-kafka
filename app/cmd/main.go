9280f87fc252f2d99b4ccc59fadd045cf728805e
emmanuelviniciusdev <emmanuelviniciusdev@gmail.com>
2023-10-27 11:07:48 -0300

    imersao-fullcycle-apache-kafka

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/emmanuelviniciusdev/imersao-fullcycle-apache-kafka/app"
+	"sync"
+	"time"
 )
 
 // Handy commands:
-// kafka-topics --create --bootstrap-server=localhost:9092 --topic=test --partitions=3
-// kafka-console-consumer --bootstrap-server=localhost:9092 --topic=test
+// kafka-topics --create --bootstrap-server=localhost:9092 --topic=test2 --partitions=5
+// kafka-console-consumer --bootstrap-server=localhost:9092 --topic=test2
 
 func main() {
 	producer, err := app.NewKakfaProducer()
@@ -19,15 +21,31 @@ func main() {
 
 	deliveryChan := make(chan kafka.Event)
 
-	err = app.Publish("Hello, Kafka c:", []byte("baz"), "test2", producer, deliveryChan)
-
-	if err != nil {
-		panic(err)
-	}
+	go publishInfiniteMessages(producer, deliveryChan)
 
 	go deliveryReport(deliveryChan)
 
-	producer.Flush(3000)
+	go app.NewKafkaConsumer([]string{"test2"}, "goapp-consumer", "goapp-consumer-group")
+	go app.NewKafkaConsumer([]string{"test2"}, "goapp-consumer", "goapp-consumer-group")
+	go app.NewKafkaConsumer([]string{"test2"}, "goapp-consumer", "goapp-consumer-group")
+	go app.NewKafkaConsumer([]string{"test2"}, "goapp-consumer", "goapp-consumer-group")
+	go app.NewKafkaConsumer([]string{"test2"}, "goapp-consumer", "goapp-consumer-group")
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	wg.Wait()
+}
+
+func publishInfiniteMessages(producer *kafka.Producer, deliveryChan chan kafka.Event) {
+	for {
+		err := app.Publish("Hello, Kafka c:", nil, "test2", producer, deliveryChan)
+
+		if err != nil {
+			panic(err)
+		}
+
+		time.Sleep(time.Second * 5)
+	}
 }
 
 func deliveryReport(deliveryChan chan kafka.Event) {
